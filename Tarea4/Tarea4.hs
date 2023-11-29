413de5fe6b53dcc4ee9b1764f897b42ab63e6449
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2023-11-29 11:48:41 -0600

    feat: insetion sort
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- Tarea4/Tarea4.hs
+++ Tarea4/Tarea4.hs
@@ -1,13 +1,19 @@
-module Matrices where
+module Tarea4 where
 
 sumaMatrices [] [] = []
 sumaMatrices (x:xs) (y:ys) = sumarRow x y : sumaMatrices xs ys
   where
     sumarRow [] [] = []
-    sumarRow (x:xs) (y:ys) = (x + y) : sumarRow
+    sumarRow (x:xs) (y:ys) = (x + y) : sumarRow xs ys
 
 
-invertirR = foldr (\ x xs -> xs ++ [x] ) []
-invertirL = foldl (\ xs x -> [x] ++ xs) []
+invertirR x = foldr (\ x xs -> xs ++ [x] ) [] x
+invertirL x = foldl (\ xs x -> [x] ++ xs) [] x
 
-mergeSort x:xs
+insertionSort [] = []
+insertionSort (x:xs) = insertar x (insertionSort xs)
+  where
+    insertar x [] = [x]
+    insertar x (y:ys)
+      | x <= y    = x : y : ys
+      | otherwise = y : insertar x ys
