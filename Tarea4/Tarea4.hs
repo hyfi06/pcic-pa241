ba88ffbf390f6ffe6f8992155eb9ede43d5f156f
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2023-11-28 18:53:47 -0600

    snapshot
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- Tarea4/Tarea4.hs
+++ Tarea4/Tarea4.hs
@@ -1,3 +1,13 @@
 module Matrices where
 
-sumaMatriz (x:xs) (y:ys) =
+sumaMatrices [] [] = []
+sumaMatrices (x:xs) (y:ys) = sumarRow x y : sumaMatrices xs ys
+  where
+    sumarRow [] [] = []
+    sumarRow (x:xs) (y:ys) = (x + y) : sumarRow
+
+
+invertirR = foldr (\ x xs -> xs ++ [x] ) []
+invertirL = foldl (\ xs x -> [x] ++ xs) []
+
+mergeSort x:xs
