2299bf191d6e3358ddd447e434540734a6de5e88
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2023-11-29 12:17:27 -0600

    feat: add ejercicios 2 y 4 de tarea 4
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- Tarea4/Tarea4.hs
+++ Tarea4/Tarea4.hs
@@ -1,14 +1,17 @@
 module Tarea4 where
 
+
+
+
+-- Ejercicio 1
+
 sumaMatrices [] [] = []
 sumaMatrices (x:xs) (y:ys) = sumarRow x y : sumaMatrices xs ys
   where
     sumarRow [] [] = []
     sumarRow (x:xs) (y:ys) = (x + y) : sumarRow xs ys
 
-
-invertirR x = foldr (\ x xs -> xs ++ [x] ) [] x
-invertirL x = foldl (\ xs x -> [x] ++ xs) [] x
+-- Ejercicio 2
 
 insertionSort [] = []
 insertionSort (x:xs) = insertar x (insertionSort xs)
@@ -17,3 +20,20 @@ insertionSort (x:xs) = insertar x (insertionSort xs)
     insertar x (y:ys)
       | x <= y    = x : y : ys
       | otherwise = y : insertar x ys
+
+compararListas xs ys = insertionSort xs == insertionSort ys
+
+-- Ejercicio 3
+invertirR x = foldr (\ x xs -> xs ++ [x] ) [] x
+invertirL x = foldl (\ xs x -> [x] ++ xs) [] x
+
+
+-- Ejercicio 4
+
+mergeList xs ys = merge (insertionSort xs) (insertionSort ys)
+  where
+    merge x [] = x
+    merge [] x = x
+    merge (x:xs) (y:ys)
+      | x <= y    = x : merge xs (y:ys)
+      | otherwise = y : merge (x:xs) ys
